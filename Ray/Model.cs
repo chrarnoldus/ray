c9f8c3132cc1e3480b10296a1ed5edc42bf3c762
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2018-12-16 21:20:06 +0100

    Remove unused usings

--- Ray/Model.cs
+++ Ray/Model.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace Ray
 {
--- Ray/RayTracer.cs
+++ Ray/RayTracer.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using YamlDotNet.Core;
 using YamlDotNet.RepresentationModel;
 
 namespace Ray
